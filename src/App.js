4e7ef911ea1702a57e2cafd2461257fed1b36e7b
alimiyan <alimiyan123@gmail.com>
2022-04-23 21:58:33 +0530

    dynamic contents

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import NavBar from './Components/NavBar/NavBar';
 import RowPost from './Components/RowPost/RowPost';
+import React from 'react';
 
 function App() {
   return (
--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Banner.css'
+import { API_Key, imageUrl } from '../../constants/constants'
+import axios from '../../axios'
 
 function Banner() {
+  const [movie, setMovie] = useState('')
+  const index = Math.floor(Math.random() * 21);
+  useEffect(() => {
+      axios.get(`trending/all/week?api_key=${API_Key}&language=en-US`).then((response)=>{
+        console.log(response.data.results[index])
+        setMovie(response.data.results[index])
+      }).catch(err=>{
+        alert('Error occured')
+      })
+  }, [])
   return (
-    <div className='banner'>
+    <div style={{backgroundImage: `url(${movie ? imageUrl+movie.backdrop_path : ""})`}} className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>{movie ? movie.original_name || movie.original_title : ""}</h1>
             <div className="banner_buttons">
                 <button className="button">Play</button>
                 <button className="button">My list</button>
             </div>
-            <h1 className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without</h1>
+            <h1 className='description'>{movie ? movie.overview : ""}</h1>
         </div>
         <div className="fade"></div>
     </div>
