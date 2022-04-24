525759765adefe8904c67a92f971a58d1e6db9e8
alimiyan <alimiyan123@gmail.com>
2022-04-24 15:00:09 +0530

    made changes

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,30 +1,59 @@
-import React,{useEffect,useState} from 'react'
-import './RowPost.css'
-import axios from '../../axios'
-import { API_Key,imageUrl } from '../../constants/constants'
+import React, { useEffect, useState } from "react";
+import "./RowPost.css";
+import axios from "../../axios";
+import YouTube from "react-youtube";
+import { imageUrl, API_Key } from "../../constants/constants";
 
-function RowPost() {
-  const [movies, setMovies] = useState([])
+function RowPost(props) {
+  const [movies, setMovies] = useState([]);
+  const [urlId, setUrlId] = useState("");
   useEffect(() => {
-    axios.get(`discover/tv?api_key=${API_Key}&with_networks=213`).then((response)=>{
-      console.log(response.data.results)
-      setMovies(response.data.results)
-    }).catch(err=>{
-      alert('Error occured')
-    })
-  }, [])
-  
+    axios
+      .get(props.url)
+      .then((response) => {
+        console.log(response.data.results);
+        setMovies(response.data.results);
+      })
+      .catch((err) => {
+        alert("Error occured");
+      });
+  }, []);
+  const opts = {
+    height: "390",
+    width: "100px",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+  const handleMovie = (id) => {
+    console.log(id);
+    axios
+      .get(`/movie/${id}/videos?api_key=${API_Key}&language=en-US`)
+      .then((response) => {
+        // console.log(response.data)
+        if (response.data.results.length !== 0) {
+          setUrlId(response.data.results[0]);
+        } else {
+          console.log("array length dont match");
+        }
+      });
+  };
   return (
-    <div className='row'>
-        <h2>Netflix Originals</h2>
-        <div className="posters">
-          {movies.map((obj)=>
-            <img className='poster' src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
-          )}
-
-        </div>
+    <div className="row">
+      <h2>{props.title}</h2>
+      <div className="posters">
+        {movies.map((obj) => (
+          <img
+            onClick={() => handleMovie(obj.id)}
+            className={props.isSmall ? "smallposter" : "poster"}
+            src={`${imageUrl + obj.backdrop_path}`}
+            alt="poster"
+          />
+        ))}
+      </div>
+      {urlId && <YouTube opts={opts} videoId={urlId.key} />}
     </div>
-  )
+  );
 }
 
-export default RowPost
+export default RowPost;
